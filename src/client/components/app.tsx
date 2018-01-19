83ef36e82bfcba23b145696730129a95be4eb7ce
rinsuki <428rinsuki+git@gmail.com>
2018-01-19 18:49:21 +0900

    /my/followers

--- src/client/components/app.tsx
+++ src/client/components/app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { APIUser } from "../../api-interfaces";
 import Header from "./header";
 import PageIndex from "./pages/index";
@@ -9,6 +9,8 @@ import PageMyQuestions from "./pages/my/questions";
 import PageUserIndex from "./pages/user/index"
 import PageUserQuestion from "./pages/user/question";
 import PageLatest from "./pages/latest";
+import { me }  from "../initial-state"
+import PageMyFollowers from "./pages/my/followers";
 
 interface Props {
     userInfo: APIUser | undefined
@@ -25,10 +27,10 @@ export default class App extends React.Component<Props> {
                     <Route exact path="/" component={PageIndex}/>
                     <Route exact path="/latest" component={PageLatest}/>
                     <Route exact path="/login" component={PageLogin}/>
-                    <Route exact path="/my">
-                        <PageMyIndex userInfo={this.props.userInfo!}/>
-                    </Route>
+                    {!me && <Redirect from="/my" to="/login"/>}
+                    <Route exact path="/my" component={PageMyIndex}/>
                     <Route exact path="/my/questions" component={PageMyQuestions}/>
+                    <Route exact path="/my/followers" component={PageMyFollowers}/>
                     <Route exact path="/@:user_id" component={PageUserIndex}/>
                     <Route exact path="/@:user_id/questions/:question_id" component={PageUserQuestion}/>
                 </Switch>
--- src/client/components/pages/my/index.tsx
+++ src/client/components/pages/my/index.tsx
@@ -2,18 +2,16 @@ import * as React from "react"
 import { Link } from "react-router-dom";
 import apiFetch from "../../../api-fetch"
 import { APIUser } from "../../../../api-interfaces"
+import { me } from "../../../initial-state"
 
-interface Props {
-    userInfo: APIUser
-}
-
-export default class PageMyIndex extends React.Component<Props> {
+export default class PageMyIndex extends React.Component {
     render() {
+        if (!me) return null
         return <div>
             <h1>マイページ</h1>
-            <p>こんにちは、{this.props.userInfo.name}さん!</p>
+            <p>こんにちは、{me.name}さん!</p>
             <ul>
-                <li><Link to={`/@${this.props.userInfo.acct}`}>あなたのプロフィール</Link></li>
+                <li><Link to={`/@${me.acct}`}>あなたのプロフィール</Link></li>
                 <li><Link to="/my/questions">あなた宛ての質問</Link></li>
                 <li><Link to="/my/followers">Quesdonを利用しているフォロワー一覧</Link></li>
                 <li><Link to="/my/settings">設定</Link></li>
