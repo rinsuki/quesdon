4463f9c39094c16d69859d8ff3bc4487d722396d
rinsuki <428rinsuki+git@gmail.com>
2018-01-20 22:06:50 +0900

    通常版ってなんだよ

--- src/client/components/pages/notfound.tsx
+++ src/client/components/pages/notfound.tsx
@@ -8,7 +8,6 @@ export default class PageNotFound extends React.Component {
             <Title>Not Found</Title>
             <h1>Not Found</h1>
             <p>指定されたページが見つかりませんでした。</p>
-            <p>未実装な機能の場合、<a href={"https://quesdon.rinsuki.net"+location.pathname}>通常版</a>を使うと動作する可能性があります。</p>
             <p><Link to="/">トップに戻る</Link></p>
         </div>
     }
