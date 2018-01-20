4e8a098017ab70bfae2e9278064ca1a99c388286
rinsuki <428rinsuki+git@gmail.com>
2018-01-20 21:35:34 +0900

    remove old client

--- src/client-old/index.ts
+++
@@ -1,8 +0,0 @@
-import * as riot from "riot"
-import "./tags/app.tag"
-import "./ctrl-enter.ts"
-(<any>window).XDate = require("xdate")
-
-addEventListener("load", () => {
-    riot.mount("app")
-})
