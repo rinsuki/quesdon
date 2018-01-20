c5481a1e85b07c9f0632f8bd56c8aa58aa8ba7a6
rinsuki <428rinsuki+git@gmail.com>
2018-01-20 23:04:58 +0900

    gitコミットのところをリンクにしたのとinitialStateにupstreamUrlを追加

--- src/client/components/footer.tsx
+++ src/client/components/footer.tsx
@@ -1,11 +1,12 @@
 import * as React from "react"
 import { Link } from "react-router-dom";
-import { usingDarkTheme, gitVersion } from "../initial-state";
+import { usingDarkTheme, gitVersion, upstreamUrl } from "../initial-state";
 
 export default class Footer extends React.Component {
     render() {
         return <footer className="container">
-            <p>Quesdon は AGPL-3.0 で提供されています。<a href="https://github.com/rinsuki/quesdon">ソースコード</a> ({gitVersion.slice(0,7)})</p>
+            <p>Quesdon は AGPL-3.0 で提供されています。<a href={upstreamUrl}>ソースコード</a>&nbsp;
+            (<a href={`${upstreamUrl}/commits/${gitVersion}`}>{gitVersion.slice(0,7)}</a>)</p>
             <p>公式アカウント: <a href="https://mstdn.jp/@quesdon">@quesdon@mstdn.jp</a> <Link to="/@quesdon@mstdn.jp">公式Quesdon (要望や不具合報告もこちらへどうぞ)</Link></p>
             <p>開発者: <a href="https://mstdn.maud.io/@rinsuki">@rinsuki@mstdn.maud.io</a></p>
             <p>
--- src/client/initial-state.ts
+++ src/client/initial-state.ts
@@ -6,4 +6,6 @@ export const me: APIUser | undefined = w.USER
 export const csrfToken: string = w.CSRF_TOKEN
 
 export const usingDarkTheme: boolean = !!localStorage.getItem("using-dark-theme")
+
+export const upstreamUrl = "https://github.com/rinsuki/quesdon"
 export const gitVersion = w.GIT_VERSION
